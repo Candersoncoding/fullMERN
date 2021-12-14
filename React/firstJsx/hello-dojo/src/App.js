6450f388f70f4632ba703dd0a95407484ff04e71
Candersoncoding <soccercda88@aol.com>
2021-12-14 02:00:24 -0600

    initial React assignments

---
+++ React/firstJsx/hello-dojo/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo!</h1>
+      </header>
+      <div>
+        <h3>Things I need to do:</h3>
+      </div>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
