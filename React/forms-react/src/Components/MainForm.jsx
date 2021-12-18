96839d8778bb09ff422a4bf67d60e8f1830f60af
Candersoncoding <soccercda88@aol.com>
2021-12-17 18:31:37 -0600

    re-formatted submit for button and form

--- React/forms-react/src/Components/MainForm.jsx
+++ React/forms-react/src/Components/MainForm.jsx
@@ -7,57 +7,63 @@ const MainForm = (props) => {
         firstName: "",
         lastName: "",
         email: "",
-        phoneNumber: parseInt(""),
+        phoneNumber: "",
         address: "",
         city: "",
         state: ""
     })
 
-    const onClickSubmit = (event) => {
+    const onChangeInput = (event) => {
         setForm({
             ...form, //spread the current data in form state
             [event.target.name]: event.target.value // get the value for the data related to the name field of each key in form state 
         });
     }
 
+    const onClickSubmit = () => {
+
+    }
+
     return(
         <div className='mainFormContainer bg-dark rounded m-5 p-5 pt-2'>
-            <h1 className='d-flex text-light content-justify-left m-5'>Customer Information</h1>
-            <div className='form-floating m-5'>
-                <input className="form-control" type="text" name="firstName" placeholder="Default input" onChange={onClickSubmit}></input>
-                <label for="floatingFirstName">First Name</label>
-            </div>
-            <div className='form-floating m-5'>
-                <input className="form-control" type="text" name="lastName" placeholder="Default input" onChange={onClickSubmit}></input>
-                <label for="floatingLastName" >Last Name</label>
-            </div>
-            <div className='form-floating m-5'>
-                <input className="form-control" type="email" name="email" placeholder="Default input" onChange={onClickSubmit}></input>
-                <label for="floatingEmail" >E-mail</label>
-            </div>
-            <div className='form-floating m-5'>
-                <input className="form-control" type="number" name="phoneNumber" placeholder="Default input" onChange={onClickSubmit}></input>
-                <label for="floatingPhoneNumber" >Phone Number</label>
-            </div>
-            <div className='d-flex m-5'>
-                <div className='form-floating m-1 flex-grow-1'>
-                    <input className="form-control" type="text" name="address" placeholder="Default input" onChange={onClickSubmit}></input>
-                    <label for="floatingAddress" >Address</label>
+            <form onSubmit={onClickSubmit}>
+                <h1 className='d-flex text-light content-justify-left m-5'>Customer Information</h1>
+                <div className='form-floating m-5'>
+                    <input className="form-control" type="text" name="firstName" placeholder="Default input" onChange={onChangeInput}></input>
+                    <label for="floatingFirstName">First Name</label>
                 </div>
-                <div className='form-floating m-1 flex-grow-0'>
-                    <input className="form-control" type="text" name="city" placeholder="Default input" onChange={onClickSubmit}></input>
-                    <label for="floatingCity" >City</label>
+                <div className='form-floating m-5'>
+                    <input className="form-control" type="text" name="lastName" placeholder="Default input" onChange={onChangeInput}></input>
+                    <label for="floatingLastName" >Last Name</label>
                 </div>
-                <div className='form-floating m-1 flex-grow-0'>
-                    <input className="form-control" type="text" name="state" placeholder="Default input" onChange={onClickSubmit}></input>
-                    <label for="floatingState" >State</label>
+                <div className='form-floating m-5'>
+                    <input className="form-control" type="email" name="email" placeholder="Default input" onChange={onChangeInput}></input>
+                    <label for="floatingEmail" >E-mail</label>
                 </div>
-            </div>
-            
-            <div className="d-flex justify-content-end m-5">
-                <button className='btn btn-outline-danger m-2'>Cancel</button>
-                <button className='btn btn-outline-success m-2' onClick={onClickSubmit}>Submit Delivery Info</button>
-            </div>
+                <div className='form-floating m-5'>
+                    <input className="form-control" type="number" name="phoneNumber" placeholder="Default input" onChange={onChangeInput}></input>
+                    <label for="floatingPhoneNumber" >Phone Number</label>
+                </div>
+                <div className='d-flex m-5'>
+                    <div className='form-floating m-1 flex-grow-1'>
+                        <input className="form-control" type="text" name="address" placeholder="Default input" onChange={onChangeInput}></input>
+                        <label for="floatingAddress" >Address</label>
+                    </div>
+                    <div className='form-floating m-1 flex-grow-0'>
+                        <input className="form-control" type="text" name="city" placeholder="Default input" onChange={onChangeInput}></input>
+                        <label for="floatingCity" >City</label>
+                    </div>
+                    <div className='form-floating m-1 flex-grow-0'>
+                        <input className="form-control" type="text" name="state" placeholder="Default input" onChange={onChangeInput}></input>
+                        <label for="floatingState" >State</label>
+                    </div>
+                </div>
+                
+                <div className="d-flex justify-content-end m-5">
+                    <button className='btn btn-outline-danger m-2'>Cancel</button>
+                    <button className='btn btn-outline-success m-2' type="submit" >Submit Delivery Info</button>
+                </div>
+            </form>
         </div>
     )
 }
