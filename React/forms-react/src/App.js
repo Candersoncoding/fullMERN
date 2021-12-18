6b9a724810a652a4c8e35fe0953ce832aad8bfb6
Candersoncoding <soccercda88@aol.com>
2021-12-17 18:07:59 -0600

    initial commit for form notes and practice

---
+++ React/forms-react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainForm from './Components/MainForm.jsx';
+
+function App() {
+  return (
+    <div className="App bg-secondary">
+      <header className="App-header bg-primary p-4 text-light border border-light border-top-0 border-right-0 border-left-0">
+        <h1>Welcome to Pizza Man!</h1>
+      </header>
+      <MainForm />
+    </div>
+  );
+}
+
+export default App;
