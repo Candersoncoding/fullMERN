6c19f06d5675c88d298bc2888e3c9630d3cf95e2
Candersoncoding <soccercda88@aol.com>
2021-12-20 00:12:44 -0600

    fixed floating label and form caption

--- React/forms-react/src/Views/PizzaForm.jsx
+++ React/forms-react/src/Views/PizzaForm.jsx
@@ -4,31 +4,32 @@ const PizzaForm = (props) =>{
 
     
     return(
-        <div>
-            <form >
-                <div className="form-floating">
-                    <input class="form-control" type="text" name="pizzaSize"></input>
-                    <label htmlFor="pizzaSize">Size</label>
+        <div className="pizzaFormContainer p-5 m-5 bg-dark rounded">
+            <form>
+                <h1 className="text-primary mb-5">Build a Pizza!</h1>
+                <div className="form-floating m-3">
+                    <input class="form-control" type="text" placeholder="Default Input" name="pizzaSize"></input>
+                    <label htmlFor="floatingpizzaSize">Size</label>
                 </div>
-                <div className="form-floating">
-                    <input class="form-control" type="text" name="pizzaSize"></input>
-                    <label htmlFor="pizzaCrust">Pizza Crust</label>
+                <div className="form-floating m-3">
+                    <input class="form-control" type="text" placeholder="Default Input" name="pizzaCrust"></input>
+                    <label htmlFor="floatingpizzaCrust">Pizza Crust</label>
                 </div>
-                <div className="form-floating">
-                    <input class="form-control" type="text" name="pizzaSize"></input>
-                    <label htmlFor="pizzaCutStyle">Cut Style</label>
+                <div className="form-floating m-3">
+                    <input class="form-control" type="text" placeholder="Default Input" name="pizzaCutStyle"></input>
+                    <label htmlFor="floatingpizzaCutStyle">Cut Style</label>
                 </div>
-                <div className="form-floating">
+                <div className="form-floating m-3">
                     <h4 className="text-light">Select Your Cheezyness!</h4>
                     <input type="range" className="form-control-range col-12"></input>
                 </div>
-                <div className="form-floating">
-                    <input class="form-control" type="text" name="pizzaSize"></input>
-                    <label htmlFor="pizzaMeats">Meats</label>
+                <div className="form-floating m-3">
+                    <input class="form-control" type="text" placeholder="Default Input" name="pizzaMeats"></input>
+                    <label htmlFor="floatingpizzaMeats">Meats</label>
                 </div>
-                <div className="form-floating">
-                    <input class="form-control" type="text" name="pizzaSize"></input>
-                    <label htmlFor="pizzaVeggies">Veggies</label>
+                <div className="form-floating m-3">
+                    <input class="form-control" type="text" placeholder="Default Input" name="pizzaVeggies"></input>
+                    <label htmlFor="floatingpizzaVeggies">Veggies</label>
                 </div>
             </form>
         </div>
