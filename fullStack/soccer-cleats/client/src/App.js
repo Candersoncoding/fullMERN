8b280c5b5bcb1caf3d767649103816cd28d6112a
Candersoncoding <soccercda88@aol.com>
2022-01-14 18:35:13 -0600

    frontend form delete with axios, remove from list

--- fullStack/soccer-cleats/client/src/App.js
+++ fullStack/soccer-cleats/client/src/App.js
@@ -3,47 +3,58 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import SoccerCleatsForm from './components/SoccerCleatsForm';
+import trash from './Images/trash_can.png';
 
 
 function App() {
 
-  // const [soccerCleats, setSoccerCleats] = useState([]);
+  const [soccerCleats, setSoccerCleats] = useState([]);
 
-  // useEffect(()=>{
-  //   axios.get('http://localhost:8000/api/soccer-cleats')
-  //     .then(res=> setSoccerCleats(res.data.soccerCleats))
-  //     .catch(err=> console.log(err))
-  // }, [])
+  useEffect(()=>{
+    axios.get('http://localhost:8000/api/soccer-cleats')
+      .then(res=> setSoccerCleats(res.data.soccerCleats))
+      .catch(err=> console.log(err))
+  }, [soccerCleats])
 
+  const onDeleteHandler = (_id, i) => {
+    console.log(_id)
+    axios.delete(`http://localhost:8000/api/soccer-cleats/delete/${_id}`)
+      .then( setSoccerCleats(soccerCleats.slice(0, i), soccerCleats.slice(i+1, soccerCleats.length-1)))
+      .catch(err=> console.log(err))
+  }
 
   return (
     <div className="App">
       <h1 className='display-1'>Soccer Cleats</h1>
-      <SoccerCleatsForm />
-      {/* <table className='table w-75 mx-auto center'>
-        <thead className='bg-dark text-light'>
-          <tr>
-            <th>Style</th>
-            <th>Color</th>
-            <th>Material</th>
-            <th>Weight</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            soccerCleats.map((item, i)=>{
-              return  <tr key={i} className='table-secondary'>
-                        <td>{item.styleName}</td>
-                        <td>{item.color}</td>
-                        <td>{item.material}</td>
-                        <td>{item.weight}</td>
-                        <td>${item.price}</td>
-                      </tr>
-            })
-          }
-        </tbody>
-      </table> */}
+      <div className='d-flex'>        
+        <table className='table w-75 mx-auto center m-3'>
+          <thead className='bg-dark text-light'>
+            <tr>
+              <th>Style</th>
+              <th>Color</th>
+              <th>Material</th>
+              <th>Weight</th>
+              <th>Price</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              soccerCleats.map((item, i)=>{
+                return  <tr key={i} className='table-secondary'>
+                          <td>{item.styleName}</td>
+                          <td>{item.color}</td>
+                          <td>{item.material}</td>
+                          <td>{item.weight}</td>
+                          <td>${item.price}</td>
+                          <td><img onClick={()=>onDeleteHandler(item._id, i)} src={trash} alt="trash can icon" className='w-25'  /></td>
+                        </tr>
+              })
+            }
+          </tbody>
+        </table>
+        <SoccerCleatsForm />
+      </div>
     </div>
   );
 }
--- fullStack/soccer-cleats/client/src/components/SoccerCleatsForm.jsx
+++ fullStack/soccer-cleats/client/src/components/SoccerCleatsForm.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, {useState, useEffect} from "react";
 
 
@@ -21,33 +22,35 @@ const SoccerCleatsForm = (props) => {
 
     const onSubmitHandler = (event) => {
         event.preventDefault();
+        axios.post("http://localhost:8000/api/soccer-cleats/create", cleatForm);
         console.log(cleatForm);
+        document.forms['formCleats'].reset();
     }
     return(
-        <div className="formContainer d-flex justify-content-end" onSubmit={onSubmitHandler}>
-            <form className="w-25 bg-dark rounded m-3">
+        <div className="formContainer d-flex justify-content-end w-25 bg-dark rounded m-3 " onSubmit={onSubmitHandler}>
+            <form className="mx-auto center" name="formCleats">
                 <h3 className="text-light m-2 display-6">Add New Cleats!</h3>
-                <div className="form-floating m-3">
+                <div className="form-floating mt-3">
                     <input class="form-control" type="text" name="styleName" placeholder="Default input" onChange={onChangeHandler}/>
                     <label htmlFor="floatingStyleName">Style Name</label>
                 </div>
-                <div className="form-floating m-3">
+                <div className="form-floating mt-3">
                     <input class="form-control" type="text" name="color" placeholder="Default input" onChange={onChangeHandler}/>
                     <label htmlFor="floatingColor">Color</label>
                 </div>
-                <div className="form-floating m-3">
+                <div className="form-floating mt-3">
                     <input class="form-control" type="text" name="material" placeholder="Default input" onChange={onChangeHandler}/>
                     <label htmlFor="floatingMaterial">Material</label>
                 </div>
-                <div className="form-floating m-3">
+                <div className="form-floating mt-3">
                     <input class="form-control" type="text" name="weight" placeholder="Default input" onChange={onChangeHandler}/>
                     <label htmlFor="floatingWeight">Weight</label>
                 </div>
-                <div className="form-floating m-3">
+                <div className="form-floating mt-3">
                     <input class="form-control" type="text" name="price" placeholder="Default input" onChange={onChangeHandler}/>
                     <label htmlFor="floatingPrice">Price</label>
                 </div>
-                <div className="d-flex justify-content-evenly m-3">
+                <div className="d-flex justify-content-evenly mt-3 mb-3">
                     <button className="btn btn-light w-25">Cancel</button>
                     <input className="btn btn-outline-light w-25" type="submit" value="Add"/>
                 </div>
