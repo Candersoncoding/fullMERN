9b04b7127bd8132a63e4c44a8dd7a691af8322f6
Candersoncoding <soccercda88@aol.com>
2022-01-22 12:28:49 -0600

    custom validations to model

--- fullStack/movieFlix/server/models/movies.model.js
+++ fullStack/movieFlix/server/models/movies.model.js
@@ -1,11 +1,42 @@
 const mongoose = require('mongoose');
 
 const MoviesSchema = new mongoose.Schema({
-    title: String,
-    genre: String,
-    rating: String,
-    releaseDate: Date(),
-    length: String
+    title: {
+        type: String,
+        required: [true, "You must enter the movie Title"],
+        validate:{ 
+            validator: (input) => {
+            return input.length >= 3;
+            },
+            message: (input) => `${input} is not enough characters. 3 characters needed minimum.`
+        }   
+    },
+    genre: {
+        type: String,
+        required: [true, "You must enter the Genre"],
+        validate:{ 
+            validator: (input) => {
+            return input.length >= 3;
+            },
+            message: (input) => `${input} is not enough characters. 3 characters needed minimum.`
+        }
+    },
+    rating: {
+        type: String,
+        required: [true, "You must enter the movie Rating"]
+    },
+    releaseDate: {
+        type: Date(),
+        required: [true, "You must enter the Release Date"]
+    },
+    inTheaters: {
+        type: Boolean,
+        required: [true, "Is the movie out yet?"]
+    },
+    length: {
+        type: Number,
+        required: [true, "You must enter how long the movie is (in minutes)"]
+    }
 })
 
 const Movies = mongoose.model("Movies", MoviesSchema);
