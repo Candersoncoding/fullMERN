f851bfa4f3bdfcd4113896ca2a5075f49ce8c3a0
Candersoncoding <soccercda88@aol.com>
2022-01-23 22:47:25 -0600

    solved hurdle race algo

--- Algos/Algos.js
+++ Algos/Algos.js
@@ -159,3 +159,48 @@ function bonAppetit(bill, k, b) {
 
 
 }
+
+// A video player plays a game in which the character competes in a hurdle race. Hurdles are of varying heights, and the 
+// characters have a maximum height they can jump. There is a magic potion they can take that will increase their maximum 
+// jump height by 1 unit for each dose. How many doses of the potion must the character take to be able to jump all of the 
+// hurdles. If the character can already clear all of the hurdles, return 0.
+
+// Example
+
+// height = [1,2,3,3,2]
+// k=1
+
+// The character can jump 1 unit high initially and must take 3-1=2 doses of potion to be able to jump all of the hurdles.
+
+// Function Description
+
+// Complete the hurdleRace function in the editor below.
+
+// hurdleRace has the following parameter(s):
+
+// int k: the height the character can jump naturally
+// int height[n]: the heights of each hurdle
+// Returns
+
+// int: the minimum number of doses required, always  or more
+
+// couple different solutions
+
+function hurdleRace(k, height) {
+    // Write your code here
+    console.log(k)
+    console.log(height)
+    let potion = 0;
+    let temp = Math.max(...height);
+    // for(let i = 0; i < height.length; i++){
+    //        temp = height[i]-k;
+    //     if(temp > potion){
+    //         potion = temp;
+    //     }
+    //     else{
+    //         console.log("you got this");
+    //     }
+    // }
+    return temp > k ?  temp-k : potion; 
+    
+}
